661acdc2b81f13bc33a9919989bed37ac0e23dd8
Trung Pham <pdbaotrung@gmail.com>
2018-12-13 20:25:51 +0700

    added console.log

--- bigbluebutton-html5/imports/ui/components/whiteboard/whiteboard-overlay/pan-zoom-draw-listener/component.jsx
+++ bigbluebutton-html5/imports/ui/components/whiteboard/whiteboard-overlay/pan-zoom-draw-listener/component.jsx
@@ -246,6 +246,8 @@ export default class PanZoomDrawListener extends React.Component {
         y: ay + (ah / 2),
       };
       const deltaX = px - midRight.x;
+      console.log(`px: ${px}, midRightX: ${midRight.x}, textWidth: ${this.activeAnnotation.annotationInfo.textBoxWidth},
+      deltaX=${deltaX}`);
       newWidth = this.activeAnnotation.annotationInfo.textBoxWidth - deltaX;
       newHeight = this.activeAnnotation.annotationInfo.textBoxHeight;
     } else if (this.state.isResizing && this.state.canVSplitOnTop) {
