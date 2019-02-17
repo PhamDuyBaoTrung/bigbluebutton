54ca9ba48b93e230233aedba4816916300df754d
trungpham <tp@getachoo.com>
2019-02-17 10:54:04 +0700

    removed redundant height

--- bigbluebutton-html5/imports/ui/components/presentation/component.jsx
+++ bigbluebutton-html5/imports/ui/components/presentation/component.jsx
@@ -160,7 +160,7 @@ export default class PresentationArea extends Component {
       <div
         style={{
           width: adjustedSizes.width,
-          height: adjustedSizes.height + 48,
+          height: adjustedSizes.height,
           backgroundColor: 'white',
         }}
       >
