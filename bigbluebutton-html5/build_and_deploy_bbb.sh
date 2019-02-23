6959a1044536939f067f6bb8ccc1c28d0494ffcc
Trung Pham <pdbaotrung@gmail.com>
2019-02-23 12:11:23 +0700

    updated script

--- bigbluebutton-html5/build_and_deploy_bbb.sh
+++ bigbluebutton-html5/build_and_deploy_bbb.sh
@@ -18,9 +18,7 @@ MONGO_URL=mongodb://127.0.0.1:27017/$APP_NAME
 echo Installing Bigbluebutton server
 echo It will take a while
 # Run as sudo
-sudo -i
-su -c wget -qO- https://ubuntu.bigbluebutton.org/bbb-install.sh | bash -s -- -v xenial-200 -s $APP_HOST -e pdbaotrung@gmail.com -t -g -c turn.blindsidenetworks.com:fkBnCMI6Uje/oogoi1EwiZpPq5s=
-exit
+su -c 'wget -qO- https://ubuntu.bigbluebutton.org/bbb-install.sh | bash -s -- -v xenial-200 -s $APP_HOST -e pdbaotrung@gmail.com -t -g -c turn.blindsidenetworks.com:fkBnCMI6Uje/oogoi1EwiZpPq5s='
 
 # Update kurento server url
 sed -i "s|\"wsUrl.*|\"wsUrl\": \"wss://$APP_HOST/bbb-webrtc-sfu\"|g" /private/config/settings-production.json
