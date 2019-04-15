9ef63c9b6d8664999ce6f7cde332b08f10b41096
Trung Pham <pdbaotrung@gmail.com>
2019-04-15 21:31:19 +0700

    fixed drag drop image

--- bigbluebutton-html5/imports/ui/components/whiteboard/annotation-factory/component.jsx
+++ bigbluebutton-html5/imports/ui/components/whiteboard/annotation-factory/component.jsx
@@ -9,6 +9,7 @@ import Rectangle from '../annotations/rectangle/component';
 import Text from '../annotations/text/container';
 import Triangle from '../annotations/triangle/component';
 import Pencil from '../annotations/pencil/component';
+import Image from '../annotations/image/component';
 
 const ANNOTATION_CONFIG = Meteor.settings.public.whiteboard.annotations;
 const DRAW_END = ANNOTATION_CONFIG.status.end;
@@ -95,5 +96,6 @@ AnnotationFactory.defaultProps = {
     text: Text,
     triangle: Triangle,
     pencil: Pencil,
+    image: Image,
   },
 };
